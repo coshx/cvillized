511499334fe5e84a5d19feec2a2c74e567b931e5
Ben Taitelbaum <ben@coshx.com>
2014-08-14 12:10:15 -0400

    starts playing with rule-based system

--- extension/javascripts/cvillized_content.js
+++ extension/javascripts/cvillized_content.js
@@ -4,15 +4,69 @@ function decodeHtml(input){
   return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
 }
 
+function Rule(data) {
+  this.description = data.description;
+  this.domain = data.domain;
+  this.search = data.search;
+  this.img = data.img;
+  this.imgAlt = data.imgAlt;
+  if (!this.imgAlt) {
+    this.imgAlt = 'cvillized replacement';
+  }
+  this.txt = data.txt;
+
+  this.html = function() {
+    var templateHtml, templateParams;
+
+    if (this.img) {
+      templateHtml = '<img src="<%= img %>" alt="<%= imgAlt %>" height="24px"/>';
+      templateParams = {
+        img: chrome.extension.getURL(this.img),
+        imgAlt: this.imgAlt
+      }
+    } else if (this.txt) {
+      templateHtml = '<%= txt %>';
+      templateParams = { txt: this.txt };
+    } else {
+      templateHtml = '<b>[hidden by cvillized]</b>';
+      templateParams = {}
+    }
+
+    return _.template(templateHtml, templateParams);
+  }
+}
+
 var cvillized = {
-  apiUrl: "https://localhost:3000/extension/analyze_comment.json",
+  rules: [
+    new Rule({
+      description: "globally turn the f-word into an f-bomb",
+      search: /fuck/ig,
+      img: 'images/f-bomb.png',
+      imgAlt: 'f-bomb'
+    }),
+    new Rule({
+      description: "globally turn poo words into poo",
+      search: /poop(?:y)?|shit(?:ty)?|crap(?:py)?/ig,
+      img: 'images/poo.png'
+    }),
+    new Rule({
+      description: "stupid is as stupid does",
+      search: '/stupid[a-z]*\b/ig',
+      txt: 'stupendous'
+    })
+  ],
+
+  /* cvillize the page and register an event listener for changes to the page */
+  init: function() {
+    cvillized.cvillize();
+    cvillized.registerDOMChangeListener();
+  },
 
   registerDOMChangeListener: function() {
     var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-    var removeComments = _.debounce(cvillized.analyzeComments, 1500, true);
+    var cvillize = _.debounce(cvillized.cvillize, 1500, true);
     var observer = new MutationObserver(function(mutations, observer) {
-      console.log("calling debounced function");
-      removeComments();
+      cvillize();
     });
     observer.observe(document, {
       subtree: true,
@@ -21,31 +75,23 @@ var cvillized = {
     });
   },
 
-  analyzeComments: function() {
-    $(".UFICommentBody").each(function(index, el) { 
-      if($(el).attr('data-cvillized') != 'true') {
-        var origHtml = $(el).html();
-        // Get thread id and comment id from from Facebook
-        $(el).attr('data-reactid').match(/.+comment([0-9]+)\_([0-9]+).+/)
-        var threadId = RegExp.$1
-        var commentId = RegExp.$2
-      	$(el).text("[cvillized is analyzing this comment...");
-        $(el).attr('data-cvillized', 'true')
-        cvillized.analyzeComment({commentHtml: origHtml, threadId: threadId, commentId: commentId}, function(newHtml) {
-          $(el).html(decodeHtml(newHtml));
-        });
-      }
-    });
+  applyRule: function(rule, rule_index) {
+    console.log("Applying rule " + rule_index + ": " + rule.description);
+    // TODO: check if rule.domain matches current page
+
+    // Find all the matches on current page and replace them
+    var bodyHtml = $("body").html();
+    bodyHtml = bodyHtml.replace(rule.search, rule.html());
+
+    $("body").html(bodyHtml);
   },
 
-  // ansync
-  analyzeComment: function(apiArgs, callback) {
-  	$.post(cvillized.apiUrl, apiArgs).done(function(data) {
-  		callback(data.replacementHtml);
-  	});
-  }
+  cvillize: function() {
+    _.each(cvillized.rules, cvillized.applyRule);
+  },
 
 };
 
-console.log("registering DOM Change Listener");
-cvillized.registerDOMChangeListener();
+$(function() {
+  cvillized.init();
+});
